3eb0aef47e5be2c7a3416504638f4f410502e104
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-01-18 19:22:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-from os import popen
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
+from os import popen;from flask import Flask, render_template;app = Flask(__name__)
 @app.route('/os/<command>')
 def os(command): return {"output": f"""{popen(f'''cd ~/ && {command.replace("¹", "/")}''').read()}"""}
