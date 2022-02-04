6849699a8ed0c1c99dccd555caf858f37f2ca160
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-02-04 17:46:24 +0100

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,2 +1,7 @@
 import requests;import sys
-while True: um="\n->";print(requests.get(f"{f'http://{sys.argv[1]}/os/'}{input(um).replace('/', '¹')}").json()["output"])
+while True: 
+	um="\n->"
+	result = requests.get(f"{f'http://{sys.argv[1]}/os/'}{input(um).replace('/', '¹')}").json()
+	if result["error"] == "": print(result["output"])
+	elif result["error"] != "" && result["output"]: print(result["error"])
+	else: print(f"""output: \n{result["output"]}\nerror: \n{result["error"]}"")
