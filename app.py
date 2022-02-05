5d53574e0876a0d3ac922f3c7169fcbbb3e90a4c
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-02-05 21:07:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from subprocess import PIPE, Popen;from flask import Flask, render_template;app = Flask(__name__)
+from subprocess import *;from flask import Flask, render_template;app = Flask(__name__)
 @app.route('/os/<command>')
-async def os(command): 
-	stdout, stderr = await Popen(f"""cd ~/ && {command.replace("¹", "/")}""", shell=True, stdout=PIPE, stderr=PIPE).communicate()
+def os(command): 
+	stdout, stderr = Popen(f"""cd ~/ && {command.replace("¹", "/")}""", shell=True, stdout=PIPE, stderr=PIPE).communicate()
 	return {"output": stdout, "error": stderr}
